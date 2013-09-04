d25f7bac6d14a02c2620e516c5cb4490bb7bc1a6
micahhausler <micah.hausler@akimbo.io>
2013-09-03 23:38:16 -0500

    added a basic photo/album model

--- lightning/thunder/models.py
+++ lightning/thunder/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Album(models.Model):
+    name = models.CharField(max_length=128)
+    slug = models.SlugField()
+    summary = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    is_live = models.BooleanField()
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=256)
+    summary = models.TextField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='photos/%Y/%m')
+    album = models.ForeignKey(Album)
+    is_live = models.BooleanField()
